9404d697b0ce194145a802078d482cb27d286f7e
Bobby DeVeaux <me@bobbyjason.co.uk>
2018-01-19 12:30:07 +0000

    updating nginx build

--- hack/build.sh
+++ hack/build.sh
@@ -23,7 +23,7 @@ test -z "$BASE_IMAGE_NAME" && {
   BASE_IMAGE_NAME="${BASE_DIR_NAME#s2i-}"
 }
 
-NAMESPACE="codecasts/"
+NAMESPACE="ukcloud/"
 
 # Cleanup the temporary Dockerfile created by docker build with version
 trap "rm -f ${DOCKERFILE_PATH}.version" SIGINT SIGQUIT EXIT
